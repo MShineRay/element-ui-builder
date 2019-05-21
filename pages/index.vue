cae6a0a941c46fd7dd11b2fcefee65fda80e9c5f
mshineray <1259056313@qq.com>
2019-05-21 18:06:42 +0800

    u--

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,11 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        aw
-      </h1>
-      <h2 class="subtitle">
-        My wicked Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+  <section class="container"></section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   }
 }
 </script>
