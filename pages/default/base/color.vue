5c33d9c8c2cc20ac2303c1bc893757f9db9625fa
mshineray <1259056313@qq.com>
2019-05-30 17:00:29 +0800

    u--color

--- pages/default/base/color.vue
+++ pages/default/base/color.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    color
+  <div class="color">
+    <h2><a href="https://element.eleme.cn/#/zh-CN/component/color" target="_blank">Color 色彩</a></h2>
+    <p>Element 为了避免视觉传达差异，使用一套特定的调色板来规定颜色，为你所搭建的产品提供一致的外观视觉感受。</p>
+    <section>
+      <h3>主色</h3>
+      <p>Element 主要品牌颜色是鲜艳、友好的蓝色。</p>
+      <p class="el-color--primary">el-color--primary</p>
+      <p class="el-color--white" style="background-color: red">el-color--white</p>
+      <p class="el-color--black">el-color--black</p>
+      <p class="el-color--success">el-color--success</p>
+      <p class="el-color--warning">el-color--warning</p>
+      <p class="el-color--danger">el-color--danger</p>
+      <p class="el-color--black">el-color--black</p>
+      <p class="el-color--info">el-color--info</p>
+    </section>
   </div>
 </template>
 
