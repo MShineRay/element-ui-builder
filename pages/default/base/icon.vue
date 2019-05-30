6092199f5e9c9c9e0b12dd331c967e739f8335f7
mshineray <1259056313@qq.com>
2019-05-30 17:13:38 +0800

    u--icon

---
+++ pages/default/base/icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2><a href="https://element.eleme.cn/#/zh-CN/component/icon" target="_blank">Icon 图标</a></h2>
+    <p>提供了一套常用的图标集合。</p>
+
+    <section>
+      <h3>使用方法</h3>
+      <p>直接通过设置类名为 el-icon-iconName 来使用即可。例如：</p>
+      <i class="el-icon-edit"></i>
+      <i class="el-icon-share"></i>
+      <i class="el-icon-delete"></i>
+      <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    </section>
+
+    <section>
+      <h3>图标集合</h3>
+    </section>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    components: {},
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style>
+</style>
