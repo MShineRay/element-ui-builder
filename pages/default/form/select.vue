8eb20ef2c2ae4b6227871b4f980f55811ff6cd20
mshineray <1259056313@qq.com>
2020-01-09 11:04:59 +0800

    u--select

--- pages/default/form/select.vue
+++ pages/default/form/select.vue
@@ -28,7 +28,27 @@
         </el-option>
       </el-select>
     </section>
-
+    <section>
+      <h3>远程搜索</h3>
+      <p>从服务器搜索数据，输入关键字进行查找</p>
+      <el-select
+        v-model="value2"
+        filterable
+        clearable
+        allow-create
+        remote
+        reserve-keyword
+        placeholder="请输入关键词"
+        :remote-method="remoteMethod"
+        :loading="loading">
+        <el-option
+          v-for="item in options2"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+    </section>
   </div>
 </template>
 
@@ -54,7 +74,30 @@
           value: '选项5',
           label: '北京烤鸭'
         }],
-        value: ''
+        value: '',
+
+
+        options2: [],
+        value2: [],
+        list: [],
+        loading: false,
+        states: ["Alabama", "Alaska", "Arizona",
+          "Arkansas", "California", "Colorado",
+          "Connecticut", "Delaware", "Florida",
+          "Georgia", "Hawaii", "Idaho", "Illinois",
+          "Indiana", "Iowa", "Kansas", "Kentucky",
+          "Louisiana", "Maine", "Maryland",
+          "Massachusetts", "Michigan", "Minnesota",
+          "Mississippi", "Missouri", "Montana",
+          "Nebraska", "Nevada", "New Hampshire",
+          "New Jersey", "New Mexico", "New York",
+          "North Carolina", "North Dakota", "Ohio",
+          "Oklahoma", "Oregon", "Pennsylvania",
+          "Rhode Island", "South Carolina",
+          "South Dakota", "Tennessee", "Texas",
+          "Utah", "Vermont", "Virginia",
+          "Washington", "West Virginia", "Wisconsin",
+          "Wyoming"]
       }
     },
     methods: {
@@ -74,9 +117,25 @@
       },
       selectBlur(event){
 
+      },
+      remoteMethod(query) {
+        if (query !== '') {
+          this.loading = true;
+          setTimeout(() => {
+            this.loading = false;
+            this.options2 = this.list.filter(item => {
+              return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
+            });
+          }, 200);
+        } else {
+          this.options2 = [];
+        }
       }
     },
     mounted() {
+      this.list = this.states.map(item => {
+        return { value: `value:${item}`, label: `${item}` };
+      });
     }
   }
 </script>
